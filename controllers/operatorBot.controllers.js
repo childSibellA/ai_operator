810ed3dd2a00d973ea1a627b4195fab0e455b440
sibel <pipiaalex7@gmail.com>
2024-09-15 19:59:01 +0400

    update

--- controllers/operatorBot.controllers.js
+++ controllers/operatorBot.controllers.js
@@ -104,38 +104,38 @@ export async function handlerFacebook(req, res) {
       const recipient_id = webhookEvent.recipient.id;
       let company = (await getCompany(recipient_id)) || [];
       console.log(company, "company");
-      if (company) {
-        const { page_access_token } = company;
+      // if (company) {
+      //   const { page_access_token } = company;
 
-        const newMessage = webhookEvent.message?.text || "";
-        await callTypingAPI(chat_id, "mark_seen", page_access_token);
-        await callTypingAPI(chat_id, "typing_on", page_access_token);
+      //   const newMessage = webhookEvent.message?.text || "";
+      //   await callTypingAPI(chat_id, "mark_seen", page_access_token);
+      //   await callTypingAPI(chat_id, "typing_on", page_access_token);
 
-        if (newMessage) {
-          // Await the delay of 2000 milliseconds (2 seconds)
-          const assistantResponse = await chatPreparation(
-            newMessage,
-            chat_id,
-            recipient_id,
-            company
-          );
-          await delay(2000);
-          if (assistantResponse && page_access_token) {
-            await facebookMsgSender(
-              chat_id,
-              assistantResponse,
-              page_access_token
-            );
-            await callTypingAPI(chat_id, "typing_off", page_access_token);
-          }
-          // Process the new message through chatPreparation
-        } else {
-          console.log(webhookEvent, "webhook");
-        }
-      } else {
-        sendToAdmin(recipient_id);
-        console.log(`new user ID is ${recipient_id}`);
-      }
+      //   if (newMessage) {
+      //     // Await the delay of 2000 milliseconds (2 seconds)
+      //     const assistantResponse = await chatPreparation(
+      //       newMessage,
+      //       chat_id,
+      //       recipient_id,
+      //       company
+      //     );
+      //     await delay(2000);
+      //     if (assistantResponse && page_access_token) {
+      //       await facebookMsgSender(
+      //         chat_id,
+      //         assistantResponse,
+      //         page_access_token
+      //       );
+      //       await callTypingAPI(chat_id, "typing_off", page_access_token);
+      //     }
+      //     // Process the new message through chatPreparation
+      //   } else {
+      //     console.log(webhookEvent, "webhook");
+      //   }
+      // } else {
+      //   sendToAdmin(recipient_id);
+      //   console.log(`new user ID is ${recipient_id}`);
+      // }
     } else {
       res.status(404).send("Event not from a page subscription");
     }
