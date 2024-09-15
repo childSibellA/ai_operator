eb79432e34693a9a9969d0dc7c2bb1692d27ad9d
sibel <pipiaalex7@gmail.com>
2024-09-15 20:08:24 +0400

    update

--- utils/db/company.handlers.js
+++ utils/db/company.handlers.js
@@ -3,7 +3,11 @@ import { Company } from "../../models/Company.js";
 export async function getCompany(recipient_id) {
   try {
     const company = await Company.findOne({ fb_chat_id: recipient_id });
-    return company;
+    if (company) {
+      return company;
+    } else {
+      return false;
+    }
   } catch (error) {
     console.error("Error fetching company:", error);
     return false;
